1109b92189ef6cd6732db2d53c31bc10490b5b0d
amr-fathi <amr.fathi85@gmail.com>
2017-05-09 23:12:11 +0200

    Added a failure test case

--- CarGrid-rest-api-module/src/test/java/com/aaghaffar/project/api/controller/NavigationControllerTest.java
+++ CarGrid-rest-api-module/src/test/java/com/aaghaffar/project/api/controller/NavigationControllerTest.java
@@ -3,7 +3,6 @@
  */
 package com.aaghaffar.project.api.controller;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,8 +18,14 @@ import com.aaghaffar.project.api.ApiApplicationContextTest;
 public class NavigationControllerTest extends ApiApplicationContextTest {
 
     @Test
-    public void testGetNavigation() throws Exception {
+    public void testGetNavigation_Success() throws Exception {
 	this.mockMvc.perform(get("/navigate/5,5:RFLFRFLF")).andExpect(status().isOk())
+		.andDo(MockMvcResultHandlers.print());
+    }
+    
+    @Test
+    public void testGetNavigation_Failure() throws Exception {
+	this.mockMvc.perform(get("/navigate/5,5:")).andExpect(status().isBadRequest())
 		.andDo(MockMvcResultHandlers.print());
     }
 
