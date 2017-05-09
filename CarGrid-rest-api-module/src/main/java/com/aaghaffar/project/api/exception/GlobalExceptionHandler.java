10f794be05aa2a5469d80c9c30d08455ccfe8f25
amr-fathi <amr.fathi85@gmail.com>
2017-05-09 23:06:30 +0200

    refactored to use JSON response wrapper

--- CarGrid-rest-api-module/src/main/java/com/aaghaffar/project/api/exception/GlobalExceptionHandler.java
+++ CarGrid-rest-api-module/src/main/java/com/aaghaffar/project/api/exception/GlobalExceptionHandler.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aaghaffar.project.api.model.NavigationError;
 import com.aaghaffar.project.exception.CarGridServiceException;
 
 /**
  * @author Amr Abdel-Ghaffar
+ * This class acts as a global exception handler, specifying the generic service level exception to expect, 
+ * And the corresponding action (an error message to be given to the user)
  *
  */
 @ControllerAdvice
@@ -21,7 +24,8 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = CarGridServiceException.class)
-    public String handleBaseException(CarGridServiceException e) {
-	return String.format("ErrorCode: %1$s, ErrorMessage: %2$s", e.getErrCode(), e.getMessage());
+    public NavigationError handleBaseException(CarGridServiceException e) {
+	return new NavigationError(e.getErrCode(), e.getMessage());
+//	return String.format("ErrorCode: %1$s, ErrorMessage: %2$s", e.getErrCode(), e.getMessage());
     }
 }
