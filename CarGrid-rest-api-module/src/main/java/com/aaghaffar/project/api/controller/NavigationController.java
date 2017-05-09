009d9723e60a64d9d74947dda80a3e73e257b184
amr-fathi <amr.fathi85@gmail.com>
2017-05-09 23:05:48 +0200

    refactored to use JSON response wrapper

--- CarGrid-rest-api-module/src/main/java/com/aaghaffar/project/api/controller/NavigationController.java
+++ CarGrid-rest-api-module/src/main/java/com/aaghaffar/project/api/controller/NavigationController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aaghaffar.project.api.model.NavigationResponse;
 import com.aaghaffar.project.exception.CarGridServiceException;
 import com.aaghaffar.project.service.NavigationService;
 
 /**
  * @author Amr Abdel-Ghaffar
- *
+ * This is the navigation controller class, defining a GET end-point for the navigation service
  */
 @RestController
 public class NavigationController {
@@ -24,8 +25,8 @@ public class NavigationController {
     NavigationService navigationService;
 
     @GetMapping("/navigate/{navStr}")
-    public String navigate(@Valid @PathVariable String navStr) throws CarGridServiceException {
-	return navigationService.navigate(navStr);
+    public NavigationResponse navigate(@Valid @PathVariable String navStr) throws CarGridServiceException {
+	return new NavigationResponse(navigationService.navigate(navStr));
     }
 
 }
